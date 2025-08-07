328df0624287ba8e10d1e4b992196eeb035cdc83
comjs <107611589+comjs@users.noreply.github.com>
2025-08-07 17:33:05 +0900

    initial commit

---
+++ dist/hbslotFetch.d.ts
@@ -0,0 +1,29 @@
+declare namespace HbslotFetch {
+    namespace slots {
+        interface Item {
+            productName: string;
+            keyword: string;
+            startDate: string;
+            endDate: string;
+            rank: number;
+            time: string;
+            mid: number;
+        }
+        interface Result {
+            list: Item[];
+        }
+    }
+}
+declare class HbslotFetch {
+    private readonly host;
+    private readonly userAgent;
+    private cookieJar;
+    private id;
+    private pw;
+    constructor(id: string, pw: string);
+    login(): Promise<void>;
+    slots(): Promise<{
+        list: HbslotFetch.slots.Item[];
+    }>;
+}
+export default HbslotFetch;
